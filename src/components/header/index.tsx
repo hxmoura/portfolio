3d676ae55b458a16a522f833e2383d34a1bea6f4
Henrique Moura <hxcmoura@outlook.com>
2025-02-19 15:17:49 -0300

    feat: add hover effect to header buttons

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -11,7 +11,7 @@ export default function Header() {
         <Link
           target="_blank"
           href="https://www.linkedin.com/in/hxmoura/"
-          className="flex gap-2 items-center bg-[#0076B2] rounded-full py-2 px-2 sm:px-3 text-white"
+          className="flex gap-2 items-center bg-[#0076B2] rounded-full py-2 px-2 sm:px-3 text-white sm:hover:-translate-y-1 transition-transform duration-300"
         >
           <span className="text-xs max-sm:hidden">Conecte-se comigo</span>
           <RiLinkedinFill size={20} />
@@ -19,13 +19,13 @@ export default function Header() {
         <Link
           target="_blank"
           href="https://github.com/hxmoura"
-          className="bg-brand-50 dark:bg-brand-800 rounded-full p-2"
+          className="bg-brand-50 dark:bg-brand-800 rounded-full p-2 sm:hover:-translate-y-1 transition-transform duration-300"
         >
           <RiGithubFill size={20} />
         </Link>
         <Link
           href="mailto:hxmoura@hotmail.com"
-          className="bg-brand-50 dark:bg-brand-800 rounded-full p-2"
+          className="bg-brand-50 dark:bg-brand-800 rounded-full p-2 sm:hover:-translate-y-1 transition-transform duration-300"
         >
           <RiAtLine size={20} />
         </Link>
