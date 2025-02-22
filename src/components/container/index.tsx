30027eec6d4876ba8d9a71a7104303a6c34343c3
Henrique Moura <hxcmoura@outlook.com>
2025-02-22 15:55:21 -0300

    fix: fix container spacing for mobile

--- src/components/container/index.tsx
+++ src/components/container/index.tsx
@@ -5,7 +5,7 @@ type ContainerProps = {
 export default function Container({ children }: ContainerProps) {
   return (
     <div className="flex justify-center">
-      <div className="max-w-xl w-full pt-20 pb-12 mx-4 min-h-screen flex flex-col">
+      <div className="max-w-xl w-full py-12 sm:pt-20 sm:pb-12 mx-4 min-h-screen flex flex-col">
         {children}
       </div>
     </div>
