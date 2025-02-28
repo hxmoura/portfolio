27fa4bf25eb9b693a3dd6d8479113feeb9a2af69
Henrique Moura <hxcmoura@outlook.com>
2025-02-28 14:21:11 -0300

    refactor: fix container component eligibility

--- src/components/container/index.tsx
+++ src/components/container/index.tsx
@@ -4,10 +4,8 @@ type ContainerProps = {
 
 export default function Container({ children }: ContainerProps) {
   return (
-    <div className="flex justify-center">
-      <div className="max-w-xl w-full py-12 sm:pt-20 sm:pb-12 mx-4 min-h-screen flex flex-col">
-        {children}
-      </div>
+    <div className="mx-auto max-w-xl w-full py-12 sm:pt-20 sm:pb-12 px-4 min-h-screen flex flex-col">
+      {children}
     </div>
   );
 }
