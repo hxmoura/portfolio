345ee26fd4fcbc66e92d9e84a1e046a61adbb160
Henrique Moura <hxcmoura@outlook.com>
2025-03-03 13:19:44 -0300

    refactor: add more area width for content

--- src/components/container/index.tsx
+++ src/components/container/index.tsx
@@ -4,7 +4,7 @@ type ContainerProps = {
 
 export default function Container({ children }: ContainerProps) {
   return (
-    <div className="mx-auto max-w-xl w-full py-12 sm:pt-20 sm:pb-12 px-4 min-h-screen flex flex-col">
+    <div className="mx-auto max-w-[608px] w-full py-12 sm:pt-20 sm:pb-12 px-4 min-h-screen flex flex-col">
       {children}
     </div>
   );
