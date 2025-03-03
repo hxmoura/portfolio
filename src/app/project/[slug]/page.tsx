afb082cc5f7ad8c3cf8da376a7c8aa339349567d
Henrique Moura <hxcmoura@outlook.com>
2025-03-03 13:11:28 -0300

    feat: add image carousel using react-slick library on project page

--- src/app/project/[slug]/page.tsx
+++ src/app/project/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import BackButton from "@/components/backButton";
 import Badge from "@/components/badge";
+import Carousel from "@/components/carousel";
 import Container from "@/components/container";
 import Footer from "@/components/footer";
 import Header from "@/components/header";
@@ -63,6 +64,20 @@ export default function Project() {
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua
         </p>
+
+        <Carousel
+          animationBlurLevel={9}
+          images={[
+            "https://images.unsplash.com/photo-1731450626260-0ca05713fdd7?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            "https://images.unsplash.com/photo-1740165886202-924690cf11e4?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            "https://images.unsplash.com/photo-1740165886179-c2be3d6447ca?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            "https://images.unsplash.com/photo-1738924349706-14d70715e236?q=80&w=1665&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            "https://images.unsplash.com/photo-1740767582687-96244e238c81?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            "https://images.unsplash.com/photo-1562749536-5642fe7bb115?q=80&w=1672&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            "https://images.unsplash.com/photo-1599619800634-52718b7f9064?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            "https://images.unsplash.com/photo-1606523080746-c6a924e7ec8c?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+          ]}
+        />
 
         <div>
           <Title animationBlurLevel={10}>Funcionalidades 💡</Title>
---
+++ src/components/carousel/index.tsx
@@ -0,0 +1,116 @@
+"use client";
+
+import animationBlur from "@/utils/animationBlur";
+import { RiArrowDropLeftLine, RiArrowDropRightLine } from "@remixicon/react";
+import Image from "next/image";
+import { useRef } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+
+type CarouselProps = {
+  animationBlurLevel?: number;
+  images: string[];
+};
+
+export default function Carousel({
+  animationBlurLevel,
+  images,
+}: CarouselProps) {
+  const dotsRef = useRef<HTMLUListElement>(null);
+  const enableScrolling = images.length >= 5;
+
+  const settings = {
+    dots: true,
+    dotsClass: "slick-custom",
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    beforeChange: (_: number, activeIndex: number) => {
+      scrollToActiveDot(activeIndex);
+    },
+    customPaging: function (i: number) {
+      return (
+        <Image
+          src={images[i]}
+          width={0}
+          height={0}
+          unoptimized
+          alt="Preview image"
+          draggable={false}
+          className="min-w-24 h-16 sm:min-w-32 sm:h-20 cursor-pointer object-cover rounded-lg"
+        />
+      );
+    },
+    appendDots: (dots: number) => (
+      <div className="relative w-full">
+        {enableScrolling && (
+          <button
+            className="absolute left-0 bottom-0 -translate-y-1/2 bg-brand-50/70 dark:bg-brand-700/70 text-brand-700 dark:text-white w-9 h-9 rounded-full z-10 items-center justify-center cursor-pointer hidden sm:flex"
+            onClick={() => handleScroll(-400)}
+          >
+            <RiArrowDropLeftLine size={36} />
+          </button>
+        )}
+
+        <ul
+          className={`flex gap-3 overflow-x-scroll mt-3 disable-scroll scroll-smooth
+            ${!enableScrolling && "justify-center"}
+          `}
+          ref={dotsRef}
+        >
+          {dots}
+        </ul>
+
+        {enableScrolling && (
+          <button
+            className="absolute right-0 bottom-0 -translate-y-1/2 w-9 h-9 bg-brand-50/70 dark:bg-brand-700/70 text-brand-700 dark:text-white rounded-full z-10 items-center justify-center cursor-pointer hidden sm:flex"
+            onClick={() => handleScroll(400)}
+          >
+            <RiArrowDropRightLine size={36} />
+          </button>
+        )}
+      </div>
+    ),
+  };
+
+  function scrollToActiveDot(index: number) {
+    if (dotsRef.current) {
+      const dots = dotsRef.current.children;
+      const activeDot = dots[index] as HTMLElement;
+
+      handleScroll(
+        activeDot.offsetLeft -
+          dotsRef.current.offsetWidth / 2 +
+          activeDot.offsetWidth / 2
+      );
+    }
+  }
+
+  function handleScroll(value: number) {
+    if (dotsRef.current) {
+      dotsRef.current.scrollLeft = value;
+    }
+  }
+
+  return (
+    <Slider
+      {...settings}
+      className={`${animationBlurLevel && animationBlur(animationBlurLevel)}`}
+    >
+      {images.map((image) => (
+        <div key={image} className="aspect-video w-full outline-none">
+          <Image
+            src={image}
+            width={0}
+            height={0}
+            unoptimized
+            alt="Slide"
+            className="w-full h-full object-cover cursor-grab rounded-lg"
+          />
+        </div>
+      ))}
+    </Slider>
+  );
+}
