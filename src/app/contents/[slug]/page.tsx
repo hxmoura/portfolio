cb7d914644f1cefaceeacd0637faee78477af68b
Henrique Moura <hxcmoura@outlook.com>
2025-03-07 18:54:10 -0300

    feat: add base structure to content page

--- src/app/contents/[slug]/page.tsx
+++ src/app/contents/[slug]/page.tsx
@@ -1,3 +1,59 @@
+import casamoura from "@/assets/images/casamoura.png";
+import BackButton from "@/components/backButton";
+import Setup from "@/components/setup";
+import StaggedAnimation from "@/components/staggedAnimation";
+import Title from "@/components/title";
+import Image from "next/image";
+
 export default function Content() {
-  return <h1>content page</h1>;
+  return (
+    <Setup>
+      <StaggedAnimation />
+      <BackButton redirect="/contents" />
+      <Title>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</Title>
+
+      <div className="space-y-7">
+        <p className="animation-blur">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industrys standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+          <br />
+          <br />
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industrys standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </p>
+        <Image
+          src={casamoura}
+          width={0}
+          height={0}
+          draggable={false}
+          className="rounded-lg w-full max-h-80 h-full object-cover animation-blur"
+          alt="content"
+        />
+        <p className="animation-blur">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industrys standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like.
+        </p>
+      </div>
+    </Setup>
+  );
 }
