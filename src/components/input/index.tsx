f4b09ef6b3792d457fc7a86b932e6c34e3dd9294
Henrique Moura <hxcmoura@outlook.com>
2025-03-21 13:28:35 -0300

    feat: update the input component to support multi-line text

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -5,20 +5,41 @@ type InputProps = {
   value: string;
   onChange: (evt: ChangeEvent) => void;
   label?: string;
+  multiline?: boolean;
 };
 
-export default function Input({ value, name, onChange, label }: InputProps) {
+export default function Input({
+  value,
+  name,
+  onChange,
+  label,
+  multiline,
+}: InputProps) {
   return (
     <div className="w-full">
-      {label && <label htmlFor={name} className="mb-1 text-sm">{label}</label>}
-      <input
-        type="text"
-        id={name}
-        value={value}
-        name={name}
-        onChange={onChange}
-        className="py-2 px-3 bg-brand-50 dark:bg-brand-900 rounded-lg w-full"
-      />
+      {label && (
+        <label htmlFor={name} className="mb-1 text-sm font-semibold">
+          {label}
+        </label>
+      )}
+      {multiline ? (
+        <textarea
+          id={name}
+          value={value}
+          name={name}
+          onChange={onChange}
+          className="py-2 px-3 bg-brand-50 dark:bg-brand-900 rounded-lg w-full min-h-32"
+        />
+      ) : (
+        <input
+          type="text"
+          id={name}
+          value={value}
+          name={name}
+          onChange={onChange}
+          className="py-2 px-3 bg-brand-50 dark:bg-brand-900 rounded-lg w-full"
+        />
+      )}
     </div>
   );
 }
