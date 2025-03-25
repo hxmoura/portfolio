cce0057457056a9d7400313580f58d7407028745
Henrique Moura <hxcmoura@outlook.com>
2025-03-25 14:30:43 -0300

    feat: add firebase method to fetch data by query

--- src/services/database.ts
+++ src/services/database.ts
@@ -7,8 +7,12 @@ import {
   DocumentData,
   getDoc,
   getDocs,
+  limit,
   onSnapshot,
+  query,
   updateDoc,
+  where,
+  WhereFilterOp,
 } from "firebase/firestore";
 
 const database = {
@@ -23,6 +27,21 @@ const database = {
     if (docSnap.exists()) {
       return { ...docSnap.data(), id: docSnap.id };
     }
+  },
+
+  async getByQuery(
+    collectionName: string,
+    docLimit: number = 99,
+    filter: { field: string; operator: WhereFilterOp; value: string },
+  ) {
+    const q = query(
+      collection(db, collectionName),
+      where(filter.field, filter.operator, filter.value),
+      limit(docLimit)
+    );
+
+    const querySnapshot = await getDocs(q);
+    return querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
   },
 
   async createDocument<T extends DocumentData>(
